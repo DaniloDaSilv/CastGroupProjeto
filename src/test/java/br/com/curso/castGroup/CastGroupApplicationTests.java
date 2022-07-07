5fd697bd17eeab7c27032ac320d224d94306cad5
DaniloDaSilv <danilodasilvadisanto@gmail.com>
2022-07-07 16:03:56 -0300

    Projeto Criado

---
+++ src/test/java/br/com/curso/castGroup/CastGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.curso.castGroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
