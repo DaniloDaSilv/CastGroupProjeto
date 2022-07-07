3e2bdbb53671dc14bee4198316d8361d4255c52b
DaniloDaSilv <danilodasilvadisanto@gmail.com>
2022-07-07 16:10:15 -0300

    Classes Curso e categoria com relacionamento

---
+++ src/main/java/br/com/curso/castGroup/entities/Categoria.java
@@ -0,0 +1,51 @@
+package br.com.curso.castGroup.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idCategoria;
+
+	@NotNull
+	private String categoria;
+
+	
+
+	public Categoria() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Categoria(long idCategoria,String categoria) {
+		super();
+		this.idCategoria = idCategoria;
+		this.categoria = categoria;
+		
+	}
+
+	public long getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(long idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	
+}
