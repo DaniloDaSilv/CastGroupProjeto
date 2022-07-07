c9ae30fca742b933fed89e33ca2d574fd2058362
DaniloDaSilv <danilodasilvadisanto@gmail.com>
2022-07-07 16:27:31 -0300

    Busca por descrição do curso

--- src/main/java/br/com/curso/castGroup/controller/CursoController.java
+++ src/main/java/br/com/curso/castGroup/controller/CursoController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +28,11 @@ public class CursoController {
 	@GetMapping
 	public ResponseEntity<List<Curso>> GetAll() {
 		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/descricao/{descricao}")
+	public ResponseEntity<List<Curso>> getByDescricao(@PathVariable String descricao) {
+		return ResponseEntity.ok(repository.getByDescricao(descricao));
 	}
 	
 	@PostMapping
